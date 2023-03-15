f75ef01e02599bb2c9b09f80c7e596df4a9ea5eb
Brad Caron <bacaron@utexas.edu>
2023-03-15 13:37:23 -0500

    reason: patches to build_dataframe

--- build_dataframe.py
+++ build_dataframe.py
@@ -13,18 +13,21 @@ def build_dataframe():
 
 	return df
 
-def update_dataframe(df,results,measure_name,subjectID):
+def update_dataframe(df,mean,sd,measure_name,subjectID):
 
 	tmp = pd.DataFrame()
 
 	structures = ['gray_matter','white_matter','csf','wholebrain']
 
-	tmp['subjectID'] = [ 'subjectID' for f in summary_measure ]
+	tmp['subjectID'] = [ subjectID for f in structures ]
 	tmp['structureID'] = structures
-	tmp[measure_name+'_mean'] = results[[0,2,4,6]]
-	tmp[measure_name+'_sd'] = results[[1,3,5,7]]
+	tmp[measure_name+'_mean'] = mean
+	tmp[measure_name+'_sd'] = sd
 
-	df = df.append(tmp,on=['subjectID','structureID'])
+	if df.empty:
+		df = pd.concat([df,tmp])
+	else:
+		df = df.merge(tmp,on=['subjectID','structureID'])
 
 	return df
 
@@ -42,7 +45,10 @@ def extract_summary_measures(gm,wm,csf,measure):
 	mean_wb = np.mean(measure[measure>0])
 	std_wb = np.std(measure[measure>0])
 
-	return [mean_gm,std_gm,mean_wm,std_wm,mean_csf,std_csf,mean_wb,std_wb]
+	mean_meas = [mean_gm,mean_wm,mean_csf,mean_wb]
+	sd_meas = [std_gm,std_wm,std_csf,std_wb]
+
+	return mean_meas, sd_meas
 	
 def main():
 
@@ -52,28 +58,28 @@ def main():
 
 	# parse inputs
 	subjectID = config['_inputs'][0]['meta']['subject']
-	gm = nib.load('gm.nii.gz')
+	gm = nib.load('gm_bin.nii.gz')
 	gm = gm.get_fdata()
-	wm = nib.load('wm.nii.gz')
-	wm = wm.get_fdata(0)
-	csf = nib.load('csf.nii.gz')
+	wm = nib.load('wm_bin.nii.gz')
+	wm = wm.get_fdata()
+	csf = nib.load('csf_bin.nii.gz')
 	csf = csf.get_fdata()
 
 	# identify measures
 	df_measures = glob.glob('*.nii.gz')
-	measure_path = [ f for f in df_measures if f not in ['gm.nii.gz','5tt.nii.gz','wm.nii.gz','csf.nii.gz'] ]
+	measure_path = [ f for f in df_measures if f not in ['gm.nii.gz','5tt.nii.gz','wm.nii.gz','csf.nii.gz','gm_bin.nii.gz','wm_bin.nii.gz','csf_bin.nii.gz'] ]
 	
 	# build initital dataframe
 	df = build_dataframe()
 
 	# loop through measures in measure_path; load data, extract summary measures, then update dataframe
 	for meas in measure_path:
-		measure_name = meas
+		measure_name = meas.split('.')[0]
 		data = nib.load(meas)
 		data = data.get_fdata()
 
-		results = extract_summary_measures(gm,wm,csf,data)
-		df = update_dataframe(df,results,measure_name,subjectID)
+		mean,sd = extract_summary_measures(gm,wm,csf,data)
+		df = update_dataframe(df,mean,sd,measure_name,subjectID)
 
 	# build output directories
 	if not os.isdir('parc-stats'):
